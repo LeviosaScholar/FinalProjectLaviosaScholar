b824e8a0501a26f44a823358a2c986b006e7f7e7
LeviosaScholar <67505962+LeviosaScholar@users.noreply.github.com>
2020-08-10 15:19:53 +0800

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -3,9 +3,9 @@ var images = [];
 var time = 2000;
 
 
-images[0] = "image1.jpg";
-images[1] = "image2.jpg";
-images[2] = "image3.jpg";
+images[0] = "slide1.jpg";
+images[1] = "slide2.jpg";
+images[2] = "slide3.jpg";
 images[3] = "image4.jpg";
 images[4] =  "image5.jpg";
 
